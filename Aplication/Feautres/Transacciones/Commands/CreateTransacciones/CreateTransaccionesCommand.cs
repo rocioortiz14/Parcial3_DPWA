19b02e0a2f1cff5cf1a00da44ab7ae60e2ce5c34
Jesiel Argueta <cheargueta982015jhos@gmail.com>
2021-06-12 23:34:46 -0600

    Agregando Carpetas y Archivos de Transacciones

---
+++ Aplication/Feautres/Transacciones/Commands/CreateTransacciones/CreateTransaccionesCommand.cs
@@ -0,0 +1,14 @@
+﻿using Application.Interfaces;
+using Application.Wrappers;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+namespace Application.Feautres.Transacciones.Commands.CreateTransacciones
+{
+    class CreateTransaccionesCommand
+    {
+    }
+}
